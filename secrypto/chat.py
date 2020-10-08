4ebfdb9e311921890dbd48f63bef56399fee9976
aswinmurali-io <aswinmurali.co@gmail.com>
2020-10-08 19:15:45 +0530

    chat implementation rough done

--- secrypto/chat.py
+++ secrypto/chat.py
@@ -1,11 +1,14 @@
 import flask
 import string
 import secrets
+import json
+import os
 
 from .globals import app, db
 from flask_cors import cross_origin
 
-BASE_URL = "https://secrypto.herokuapp.com"
+# BASE_URL = "https://secrypto.herokuapp.com"
+BASE_URL = 'http://127.0.0.1:5000'
 
 
 def generate_uid() -> str:
@@ -44,8 +47,17 @@ def new_chatroom() -> str:
 @app.route('/chat', methods=['POST', 'GET'])
 @cross_origin()
 def chat() -> str:
-    return dict(flask.request.form)
-    # if 'username' not in flask.session:
-    #     flask.session['username'] = generate_uid()
-    # query: dict = dict(flask.request.form)['query']
-    # return flask.jsonify({"response": f'{flask.session["username"]} {query}'})
+    if flask.request.method == 'POST':
+        data = dict(flask.request.form)
+        json_file = open(data['Chat ID'], 'w')
+        json_file.write(json.dumps(data))
+        json_file.close()
+        return data
+    else:
+        chatid = flask.request.args.get('id')
+        if os.path.exists(chatid):
+            json_file = open(chatid)
+            content = json_file.read()
+            json_file.close()
+            return content
+    return '{}'
